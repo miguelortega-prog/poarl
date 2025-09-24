08e1f9fc057bbb1f1a5da64493b8dbd28c4074d8
Miguel Leonardo Ortega <miguel.ortega@segurosbolivar.com>
2025-09-24 10:26:59 -0500

    fix: errors in modal and doble import from alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,10 @@
 import './bootstrap';
 import '../css/app.css';
 
-import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
-import './plugins/trap';
+import focus from '@alpinejs/focus';
 
-Alpine.plugin(collapse);
-
-window.Alpine = Alpine;
-
-window.deferLoadingAlpine = (callback) => {
-    document.addEventListener('livewire:init', callback, { once: true });
-};
-
-Alpine.start();
+document.addEventListener('alpine:init', () => {
+    window.Alpine.plugin(collapse);
+    window.Alpine.plugin(focus);
+});
